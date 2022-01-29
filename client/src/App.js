3228c324b4adbe825a909668e49c8811e9eef219
Khang Bui <khangbui@ufl.edu>
2022-01-29 16:06:57 -0500

    testing webapge

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Menu from './Menu.js';
+import TextField from '@mui/material/TextField';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Menu></Menu>
       </header>
+      <TextField id="outlined-basic" label="Search" variant="outlined" />
     </div>
   );
 }
