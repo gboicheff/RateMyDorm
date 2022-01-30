2b0a823d683605e77e3a51e47cf868dd53bd4dfb
Grant Boicheff <gboicheff@gmail.com>
2022-01-29 19:37:55 -0500

    adding app router

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Redirect, Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+                <Route path="/" exact>
+                    
+                </Route>
+                <Route path="/login" exact>
+
+                </Route>
+                <Route path="/account" exact>
+                    
+                </Route>
+                <Route path="/signup" exact>
+                    
+                </Route>
+
+                <Route path="/home" exact>
+                    
+                </Route>
+      </BrowserRouter>
     </div>
   );
 }
