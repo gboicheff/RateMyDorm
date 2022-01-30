2b595b760dd8cce7af67d159924b4791f2cc1b3e
Grant Boicheff <gboicheff@gmail.com>
2022-01-29 23:32:06 -0500

    fixing depedencies

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = () => {
-    return mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true } );
+    return mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true} );
 };
 
 const models = {Reviews, Dorms};
